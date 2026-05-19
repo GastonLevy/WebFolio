345027fe73e0b138ce89c6dbf9020075ad210445
Gaston <ggastlino@gmail.com>
2026-05-19 09:50:07 +0200

    feat: implement hero section with content, styles, and component logic

--- frontend/src/app/sections/hero/hero.ts
+++ frontend/src/app/sections/hero/hero.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+  coreStack = ['Angular', 'Symfony', 'TypeScript', 'Docker', 'MySQL'];
+
+  infrastructureStack = ['Nginx', 'Linux', 'VPS', 'CI/CD', 'Reverse Proxy'];
+
+  additionalStack = ['Django', 'Streaming Systems', 'Multi-tenant SaaS'];
+}
