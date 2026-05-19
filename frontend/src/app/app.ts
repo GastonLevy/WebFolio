c667ca61434f1162163c7720f2abbcbb921db154
Gaston <ggastlino@gmail.com>
2026-05-19 16:15:33 +0200

    feat: update app structure by removing Hero and Projects components from imports and template

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
-import { Hero } from './sections/hero/hero';
-import { Projects } from './sections/projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero, Projects],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
